f754c5ae1d536a8cf2bc8020ac97230d95f5da08
Nemanja <nemanja.grgic@payspot.co.rs>
2026-05-21 10:09:53 +0200

    Remove ignored files from tracking

--- scripts/build-release.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-# Full release: PyInstaller executable + .deb package (same machine arch as target).
-set -e
-cd "$(dirname "$0")/.."
-
-./scripts/build-pyinstaller.sh
-./scripts/build-deb.sh
